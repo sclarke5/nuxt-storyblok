1945282b3bd747b89d765c91fa12ba1a1e92035d
sclarke5 <sclarke5555@gmail.com>
2020-11-04 15:48:12 -0500

    fix compilation errors

--- components/Feature.vue
+++ components/Feature.vue
@@ -1,8 +1,8 @@
 <template>
   <div
     v-editable="blok"
-    class="px-2">
-    <h1 class="text-lg">{{ blok.name }} </h1>
+    class="py-2">
+    <h1 class="text-lg">{{ blok.name }}</h1>
   </div>
 </template>
 
--- components/Grid.vue
+++ components/Grid.vue
@@ -4,7 +4,7 @@
     class="flex py-8 mb-6">
     <li
       :key="blok._uid"
-      :v-for="blok in blok.columns"
+      v-for="blok in blok.columns"
       class="flex-auto px-6">
       <component :blok="blok" :is="blok.component" />
     </li>
