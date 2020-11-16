cc59db7a2638837c0ed6da40867f502e2d11a940
sclarke5 <sclarke5555@gmail.com>
2020-11-16 12:51:36 -0500

    add slider images

---
+++ components/Slide.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    v-editable="blok"
+    class="py-2 grid gap-4 justify-items-center">
+    <img 
+      class="h-20 w-20"
+      :src="blok.image" />
+    <h1 class="text-lg">{{ blok.alt }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    blok: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
--- plugins/components.js
+++ plugins/components.js
@@ -5,6 +5,9 @@ import Grid from '~/components/Grid.vue'
 import Feature from '~/components/Feature.vue'
 import FeaturedArticles from '~/components/FeaturedArticles.vue'
 import ArticleTeaser from '~/components/ArticlesTeaser.vue'
+import Slider from '~/components/Slider.vue'
+import Slide from '~/components/Slide.vue'
+
 
 Vue.component('page', Page)
 Vue.component('teaser', Teaser)
@@ -12,3 +15,5 @@ Vue.component('grid', Grid)
 Vue.component('feature', Feature)
 Vue.component('featured-articles', FeaturedArticles)
 Vue.component('article-teaser', ArticleTeaser)
+Vue.component('slider', Slider)
+Vue.component('slide', Slide)
