7b8581969fb74ccc46ef074062041a7feca99e68
sclarke5 <sclarke5555@gmail.com>
2020-11-17 13:14:45 -0500

    add slider and load images via cms

--- components/Slider.vue
+++ components/Slider.vue
@@ -1,18 +1,50 @@
 <template>
-  <ul
-    v-editable="blok"
-    class="flex py-8 mb-6">
-    <li
-      :key="blok._uid"
-      v-for="blok in blok.slides"
-      class="flex-auto px-6">
-      <component :blok="blok" :is="blok.component" />
-    </li>
-  </ul>
+  <div class="slider-div">
+    <VueSlickCarousel
+      v-bind="settings"
+      v-editable="blok">
+      <div 
+        :key="blok._uid"
+        v-for="blok in blok.slides">
+        <component :blok="blok" :is="blok.component" class="slider-image"></component>
+      </div>
+    </VueSlickCarousel>
+  </div>
 </template>
 
+<style>
+  .slider-image img {
+    height: auto;
+    width: 100%;
+  }
+  .slider-div {
+    padding-top: 0px;
+    margin-top: -80px;
+  }
+  .slick-prev:before {
+    color: black;
+  }
+  .slick-next:before {
+    color: black;
+  }
+</style>
+
 <script>
+import VueSlickCarousel from 'vue-slick-carousel'
+// optional style for arrows & dots
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+
 export default {
+  data(){
+    return {
+      settings: {
+        dots: true,
+        slidesToShow: 3
+      }
+    }
+  },
+  components: { VueSlickCarousel },
   props: {
     blok: {
       type: Object,
--- components/Teaser.vue
+++ components/Teaser.vue
@@ -1,13 +1,37 @@
 <template>
   <div
     v-editable="blok"
-    class="py-8 mb-6 text-5xl font-bold text-center">
+    class="py-8 mb-6 text-5xl font-bold text-center container">
     <!-- <h1>{{blok.title}}</h1> -->
     <img 
       class="h-60 w-screen mb-4 object-cover"
       :src="blok.image.filename" />
+    <h1 class="hero-header">{{ blok.header}}</h1>
+    <h2 class="hero-intro">{{ blok.intro }}</h2>
   </div>
 </template>
+
+<style>
+  .hero-header {
+    position: absolute;
+    top: 35%;
+    left: 150px;
+    padding-right: 400px;
+    color: white;
+    font-size: 80px;
+    font-family: Impact, Charcoal, sans-serif;
+  }
+  .hero-intro {
+    position: absolute;
+    top: 35%;
+    right: 50px;
+    padding-left: 600px;
+    color: white;
+    font-size: 30px;
+    font-family: Impact, Charcoal, sans-serif;
+  }
+
+</style>
 
 <script>
 export default {
